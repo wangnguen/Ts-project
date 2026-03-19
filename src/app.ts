672851f83b58fb38d4162862504d6daa940d3955
Chung Nguyen Thanh 1 <chung.nguyen3@ntq-solution.com.vn>
2026-03-19 10:34:40 +0700

    feat: integrate TypeORM with PostgreSQL, and configure SWC for TypeScript execution.

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import rateLimit from 'express-rate-limit'
 import env from '@common/config/env.js'
 import ErrorMiddleware from '@common/middlewares/error.middleware.js'
 import ResponseMiddleware from '@common/middlewares/response.middleware.js'
+import AppDataSource from 'src/databases/data-source.js'
 
 const app: express.Application = express()
 
@@ -48,6 +49,11 @@ app.use(
 app.use(express.json({ limit: '1mb' }))
 app.use(express.urlencoded({ extended: true, limit: '1mb' }))
 app.use(ResponseMiddleware.extendResponse)
+
+AppDataSource.connect().catch((err) => {
+  console.log(`Connect to db error: ${err}`)
+  process.exit(0)
+})
 
 // Routes
 app.get('/health', (req: Request, res: Response) => {
---
+++ src/databases/data-source.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata'
+import { User } from 'src/databases/entities/user.entity.js'
+import { DataSource } from 'typeorm'
+
+class AppDataSource {
+  private static instance: DataSource
+
+  private static getInstance(): DataSource {
+    if (!AppDataSource.instance) {
+      AppDataSource.instance = new DataSource({
+        type: 'postgres',
+        url: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false
+        },
+        entities: [User],
+        migrations: ['src/migrations/*.{ts,js}'],
+        synchronize: true,
+        logging: false
+      })
+    }
+
+    return AppDataSource.instance
+  }
+
+  public static async connect(): Promise<DataSource> {
+    const dataSource = AppDataSource.getInstance()
+
+    if (!dataSource.isInitialized) {
+      await dataSource.initialize()
+      console.log('Database connected')
+    }
+
+    return dataSource
+  }
+}
+
+export default AppDataSource
