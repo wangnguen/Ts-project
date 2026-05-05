1686d977a643e5f867880d0196c7e33e23bb7fc8
Quang Nguyễn <wangnguenlc79@gmail.com>
2026-05-06 00:01:30 +0700

    feat: add Resend email configuration and update environment schema

--- src/common/config/env.ts
+++ src/common/config/env.ts
@@ -8,6 +8,7 @@ const envSchema = z.object({
   JWT_SECRET: z.string().min(32),
   JWT_REFRESH_SECRET: z.string().min(32),
   REFRESH_TOKEN_HASH_SECRET: z.string().min(32),
+  AUTH_TOKEN_HASH_SECRET: z.string().min(32),
   JWT_ACCESS_EXPIRES_IN: z.coerce
     .number()
     .int()
@@ -25,7 +26,15 @@ const envSchema = z.object({
     .default(30 * 24 * 60 * 60),
   GOOGLE_CLIENT_ID: z.string().min(1),
   GOOGLE_CLIENT_SECRET: z.string().min(1),
-  GOOGLE_CALLBACK_URL: z.url().default('http://localhost:8080/api/v1/auth/google/callback')
+  GOOGLE_CALLBACK_URL: z.url().default('http://localhost:8080/api/v1/auth/google/callback'),
+  RESEND_FROM: z.email().default('onboarding@resend.dev'),
+  RESEND_API_KEY: z.string().min(1),
+  RESEND_RECIPIENTS_EMAIL: z.email().default(''),
+  RESEND_VERIFY_TEMPLATE_ID: z.string().min(1),
+  RESEND_RESET_PASSWORD_TEMPLATE_ID: z.string().min(1),
+  APP_NAME: z.string().min(1).default('Ts Project'),
+  VERIFY_EMAIL_EXPIRE_MINUTES: z.coerce.number().int().positive().default(10),
+  RESET_PASSWORD_EXPIRE_MINUTES: z.coerce.number().int().positive().default(5)
 })
 
 export type EnvSchema = z.infer<typeof envSchema>
--- src/common/constants/auth.constant.ts
+++ src/common/constants/auth.constant.ts
@@ -11,3 +11,11 @@ export const GOOGLE_AUTH = {
   VERIFY_TOKEN_URL: 'https://oauth2.googleapis.com/token',
   USERINFO_URL: 'https://www.googleapis.com/oauth2/v2/userinfo'
 }
+
+export const EMAIL_TEMPLATE = {
+  VERIFY_EMAIL: process.env.RESEND_VERIFY_TEMPLATE_ID!,
+  RESET_PASSWORD: process.env.RESEND_RESET_PASSWORD_TEMPLATE_ID!
+} as const
+
+export const VERIFY_EMAIL_EXPIRE_MINUTES = Number(process.env.VERIFY_EMAIL_EXPIRE_MINUTES)
+export const RESET_PASSWORD_EXPIRE_MINUTES = Number(process.env.RESET_PASSWORD_EXPIRE_MINUTES)
